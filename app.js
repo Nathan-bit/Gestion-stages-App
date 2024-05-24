7cc275a116c0be90cbe6034a976a3f994ff94fea
Nathan-bit <gabiamsamuelnathan@gmail.com>
2024-05-25 00:31:12 +0100

    "Refactored Explore and Home screens to use React Native components, removed custom components and styles, and added images, modal, and button components."

---
+++ app.js
@@ -0,0 +1,12 @@
+import {View , Text,Image} from 'react-native'
+
+export default function myApp()
+{
+    return(
+        <View style={{flex:1}}>
+
+            <Text>Hello word</Text>
+
+        </View>
+    )
+}
